afc877b6c2d1d8074df9c330bde1b038597c9a30
Ibe Christopher Obinna <86110271+Etiti27@users.noreply.github.com>
2023-08-22 21:09:36 +0100

    Delete MongoDB directory

--- MongoDB/FirstMagSchema.js
+++
@@ -1,8 +0,0 @@
-const mongoose= require('mongoose');
-const firstMagSchema= new mongoose.Schema({
-    username:String,
-    name:String
-})
-
-const FirstMagModel = mongoose.model('First', firstMagSchema);
-module.exports = FirstMagModel;
--- MongoDB/MongoDB.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-exports.DBmongo=()=>{
-     mongoose.connect(`mongodb+srv://${process.env.DBusername}:${process.env.DBpassword}@cluster0.qqlrjp5.mongodb.net/SheExistMagazine`)
-    .then(() => console.log('Connected!'));
-}
-
